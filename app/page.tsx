28d541b35cba7c74460445014107f86c5316815e
Agustin Arroyo <agustin@arroyosotomayor.com>
2024-04-05 16:37:04 -0600

    titles

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+//  npm run dev 
+
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -9,8 +11,16 @@ import Image from 'next/image';
 import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
+
+  const title = "Main | Acme";
+
+
   return (
+    
     <main className="flex min-h-screen flex-col p-6">
+      
+      <title>{title}</title>      
+
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
@@ -33,6 +43,7 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
+
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
               <Image
               src="/hero-desktop.png"
